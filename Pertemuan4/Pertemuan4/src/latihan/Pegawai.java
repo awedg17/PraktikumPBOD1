8286cb7a8957d4c4769f2cf2e93cf844a0706603
Dewangga Ramadhan Halim <dewanggahalim06@gmail.com>
2026-03-13 03:23:17 +0700

    pertemuan4 pbo sudone

---
+++ Pertemuan4/Pertemuan4/src/latihan/Pegawai.java
@@ -0,0 +1,66 @@
+/* Nama File    : Pegawai.java
+ * Deskripsi    : berisi atribut dan method di class Pegawai
+ * Pembuat      : Dewangga Ramadhan Halim
+ * Tanggal      : 12 Maret 2026 
+ */
+
+package latihan;
+
+public class Pegawai {
+
+  protected String nip;
+  protected String nama;
+  protected String tanggalLahir;
+  protected String tmt;
+  protected double gajiPokok;
+
+  public Pegawai(){}
+
+  public Pegawai(String nip, String nama, String tanggalLahir, String tmt, double gajiPokok){
+    this.nip = nip;
+    this.nama = nama;
+    this.tanggalLahir = tanggalLahir;
+    this.tmt = tmt;
+    this.gajiPokok = gajiPokok;
+  }
+
+  public String getNip(){
+    return nip;
+  }
+
+  public String getNama(){
+    return nama;
+  }
+
+  public String getTanggalLahir(){
+    return tanggalLahir;
+  }
+
+  public String getTmt(){
+    return tmt;
+  }
+
+  public double getGajiPokok(){
+    return gajiPokok;
+  }
+
+  public int hitungMasaKerja() {
+    int tahunSekarang = 2026;
+    int tahunMasuk = Integer.parseInt(tmt.substring(6));
+    return tahunSekarang - tahunMasuk;
+  }
+
+  public String hitungTanggalPensiun(int bup) {
+    int tahunLahir = Integer.parseInt(tanggalLahir.substring(6));
+    int tahunPensiun = tahunLahir + bup + 1;
+    return "01-" + tanggalLahir.substring(3,5) + "-" + tahunPensiun;
+  }
+
+  public void printInfo(){
+    System.out.println("NIP : " + nip);
+    System.out.println("Nama : " + nama);
+    System.out.println("Tanggal Lahir : " + tanggalLahir);
+    System.out.println("TMT : " + tmt);
+    System.out.println("Gaji Pokok : " + gajiPokok);
+  }
+}
