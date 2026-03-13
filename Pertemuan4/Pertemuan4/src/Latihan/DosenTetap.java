610b4acecbe7f29068532643f30415179a88d87a
Dewangga Ramadhan Halim <dewanggahalim06@gmail.com>
2026-03-13 23:15:41 +0700

    pert4 done

---
+++ Pertemuan4/Pertemuan4/src/Latihan/DosenTetap.java
@@ -0,0 +1,29 @@
+/* Nama File   : Dosen.java
+* Deskripsi    : berisi atribut dan method di class Dosen
+* Pembuat      : Dewangga Ramadhan Halim
+* Tanggal      : 12 Maret 2026
+*/
+
+package Latihan;
+
+public class DosenTetap extends Dosen {
+
+  private String nidn;
+
+  public DosenTetap(String nip, String nama, String tanggalLahir, String tmt, double gajiPokok, String fakultas, String nidn){
+    super(nip, nama, tanggalLahir, tmt, gajiPokok, fakultas);
+    this.nidn = nidn;
+  }
+
+  public double hitungTunjangan(){
+    return 0.02 * hitungMasaKerja() * gajiPokok;
+  }
+
+  public void printInfo(){
+    super.printInfo();
+    System.out.println("NIDN : " + nidn);
+    System.out.println("Masa Kerja : " + hitungMasaKerja() + " tahun");
+    System.out.println("Tanggal Pensiun : " + hitungTanggalPensiun(65));
+    System.out.println("Tunjangan : " + hitungTunjangan());
+  }
+}
