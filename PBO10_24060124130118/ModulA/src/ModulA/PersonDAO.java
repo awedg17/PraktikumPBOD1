ff1781ed787f0f65fcd14499c1c8926dc953a205
Dewangga Ramadhan Halim <dewanggahalim06@gmail.com>
2026-05-15 19:49:54 +0700

    pert 10 sudone

---
+++ PBO10_24060124130118/ModulA/src/ModulA/PersonDAO.java
@@ -0,0 +1,14 @@
+/**
+ * File : PersonDAO.java
+ * Deskripsi : interface untuk person access object
+ */
+         
+package ModulA;
+
+/**
+ *
+ * @author Dewangga Ramadhan H
+ */
+public interface PersonDAO {
+    public void savePerson(Person p) throws Exception;
+}
