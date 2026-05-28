2f4f9391da0a3781f9441898dbcd1437a7600df1
Dewangga Ramadhan Halim <dewanggahalim06@gmail.com>
2026-05-28 17:16:44 +0700

    Pert 11 sudone

---
+++ src/ModulB/utilities/MysqlUtility.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ModulB.utilities;
+
+/**
+ *
+ * @author Dewangga Ramadhan H
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MysqlUtility {
+
+    private static Connection koneksi;
+
+    public static Connection getConnection() {
+        if (koneksi == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                String url = "jdbc:mariadb://localhost:3306/jdbc_mhs";
+                
+                String user = "root";
+                String password = "";
+
+                koneksi = DriverManager.getConnection(url, user, password);
+
+                if (koneksi != null) {
+                    System.out.println("Koneksi berhasil");
+                }
+
+            } catch (ClassNotFoundException cne) {
+                System.out.println("Gagal load driver : " + cne.getMessage());
+
+            } catch (SQLException sqle) {
+                System.out.println("Gagal Koneksi : " + sqle.getMessage());
+            }
+        }
+
+        return koneksi;
+    }
+}
